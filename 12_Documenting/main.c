36dad9f100dfd1dc0a5112ca1bbd44e613111dd8
Werozel <khaibulaev.mn@gmail.com>
2022-12-28 13:41:05 +0300

    Added task 12

---
+++ 12_Documenting/main.c
@@ -0,0 +1,76 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+
+const char roman[100] = { "N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV",
+                          "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI",
+                          "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI",
+                          "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII",
+                          "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX",
+                          "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                          "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                          "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX",
+                          "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+                        };
+
+const char* toRoman(int number)
+{
+    if (number < 1 || number > 100)
+    {
+        return _("Invalid number");
+    }
+    return &roman[number];
+}
+
+int guess(int l, int r, int isRoman) {
+    if (l == r) {
+        printf("Your number is %d\n", l);
+        return 1;
+    }
+
+    int m = (l + r) / 2;
+
+    printf("%s %d? (%s/%s)\n", _("Is your number greater than"), m, _("yes"), _("no"));
+    char *line = NULL;
+    size_t len = 0;
+    int read;
+    while ((read = getline(&line, &len, stdin)) > 0) {
+        line[read - 1] = '\0';
+        if (strcmp(line, _("yes")) == 0) {
+            if (m == r) {
+                printf("%s %d\n", _("Your number is"), m);
+                return 1;
+            }
+            return guess(m + 1, r) + 1;
+        } else if (strcmp(line, _("no")) == 0) {
+            if (m == l) {
+                printf("%s %d\n", _("Your number is"), m);
+                return 1;
+            }
+            return guess(l, m) + 1;
+        } else {
+            printf("%s %s/%s\n", _("Invalid input, expected"), _("yes"), _("no"));
+        }
+    }
+
+    printf("%s\n", _("Error reading input"));
+    exit(1);
+}
+
+int main(int c, const char *v[]) {
+    // todo parse -r and --help
+
+    setlocale(LC_ALL, "");
+    bindtextdomain("main", getenv("PWD"));
+    textdomain ("main");
+
+    int number_of_guesses = guess(1, 100);
+
+    printf("%s %d\n", _("Number of guesses"),  number_of_guesses);
+
+    return 0;
+}
